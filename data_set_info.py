a037e51ecbfeaa1334611e306f488b49fc86f15a
kvist3n <martin.hkvist@gmail.com>
2019-02-06 16:32:05 +0100

    extended info script

--- data_set_info.py
+++ data_set_info.py
@@ -4,11 +4,31 @@ import matplotlib.pyplot as plt
 train_df = pd.read_csv('Data/training_data.csv')
 train_df.info()
 print(train_df)
-L = train_df.columns
+L = train_df.columns.values
+print(L)
 
 print("durations min max: ", min(train_df["duration"].values), max(train_df["duration"].values))
 print("loudness min max: ", min(train_df["loudness"].values), max(train_df["loudness"].values))
 print("tempo min max: ", min(train_df["tempo"].values), max(train_df["tempo"].values))
+print("time_signature min max: ", min(train_df["time_signature"].values), max(train_df["time_signature"].values))
+
+print("instrumentalness min max: ", min(train_df["instrumentalness"].values), max(train_df["instrumentalness"].values))
+pd.plotting.scatter_matrix(train_df)
+plt.show()
+
+plt.hist(train_df["instrumentalness"])
+plt.show()
+
+train_df = pd.read_csv('Data/songs_to_classify.csv')
+train_df.info()
+print(train_df)
+L = train_df.columns.values
+print(L)
+
+print("durations min max: ", min(train_df["duration"].values), max(train_df["duration"].values))
+print("loudness min max: ", min(train_df["loudness"].values), max(train_df["loudness"].values))
+print("tempo min max: ", min(train_df["tempo"].values), max(train_df["tempo"].values))
+print("time_signature min max: ", min(train_df["time_signature"].values), max(train_df["time_signature"].values))
 print("instrumentalness min max: ", min(train_df["instrumentalness"].values), max(train_df["instrumentalness"].values))
 pd.plotting.scatter_matrix(train_df)
 plt.show()
